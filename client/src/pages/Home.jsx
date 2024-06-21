20bc352d8024a7b509d403625db84916ca07f7d0
Ram Asodhi <ramcharan2510@gmail.com>
2024-06-21 08:53:55 -0400

    Include icons in the side div

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,64 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState, useEffect } from 'react';
 import myPicture from '/src/assets/SD.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faNodeJs,
+  faReact,
+  faJs,
+  faHtml5,
+  faCss3Alt,
+  faAws,
+  faDocker,
+  faLinux,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faDatabase,
+  faNetworkWired,
+  faCloud,
+} from '@fortawesome/free-solid-svg-icons';
+
+const icons = [
+  { icon: faNodeJs, color: '#68A063' },
+  { icon: faReact, color: '#61DAFB' },
+  { icon: faJs, color: '#F7DF1E' },
+  { icon: faHtml5, color: '#E34F26' },
+  { icon: faCss3Alt, color: '#1572B6' },
+  { icon: faAws, color: '#FF9900' },
+  { icon: faDocker, color: '#2496ED' },
+  { icon: faLinux, color: '#E34F26' },
+  { icon: faDatabase, color: '#4DB33D' },
+  { icon: faNetworkWired, color: '#0071C5' },
+  { icon: faCloud, color: '#00BFFF' },
+];
+
+const getRandomPositions = () => {
+  return icons.map(() => {
+    const top = Math.random() * 80 + 0;
+    const left = Math.random() * 80 + 10;
+    return { top: `${top}%`, left: `${left}%` };
+  });
+};
 
 const Home = forwardRef((props, ref) => {
+  const [positions, setPositions] = useState(getRandomPositions());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPositions(getRandomPositions());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div ref={ref}>
       <div className="relative flex flex-col md:flex-row h-screen">
         <div className="md:h-screen h-5/6 md:w-4/5 flex items-center bg-[#97bdf7]">
-          <div className="text-center md:text-left text-3xl md:text-7xl p-8 font-serif text-white">
-            <div className="text-xl">I'm a</div>
+          <div className="text-center md:text-left text-5xl md:text-7xl p-8 font-serif text-white">
+            <div className="text-2xl md:text-pretty md:text-3xl lg:text-xl">
+              I'm a
+            </div>
             Full Stack Software Developer
-            <div className="text-xl mt-4 md:text-pretty">
+            <div className="text-2xl mt-4 md:text-pretty md:text-3xl lg:text-xl">
               with over 4 years of experience in building and maintaining web
               applications using the <br />
               MERN stack (MongoDB, Express.js, React.js, Node.js). <br />
@@ -17,12 +66,21 @@ const Home = forwardRef((props, ref) => {
             </div>
           </div>
         </div>
-        <div className="md:h-screen h-2/6 md:w-1/5 flex items-center bg-[#b3f68e]">
-          <div className="text-center md:text-left text-3xl md:text-5xl p-8 font-serif text-white">
-            <hr className="mt-10 mx-auto md:mx-10 w-1/2" />
-          </div>
+        <div className="md:h-full h-1/5 md:w-1/5 flex items-center bg-[#b3f68e] relative overflow-hidden">
+          {icons.map((iconObj, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={iconObj.icon}
+              className="text-4xl absolute"
+              style={{
+                top: positions[index].top,
+                left: positions[index].left,
+                color: iconObj.color,
+              }}
+            />
+          ))}
         </div>
-        <div className="absolute top-2/3 left-1/2 mt-6 ml-14 md:top-1/3 md:mt-28  md:left-3/4 lg:top-1/3 lg:left-3/4 lg:ml-14 lg:mt-40 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="hidden lg:block absolute top-2/3 left-1/2 mt-6 md:top-1/3 md:mt-28 md:left-3/4 lg:top-1/3 lg:left-3/4 lg:ml-14 lg:mt-40 transform -translate-x-1/2 -translate-y-1/2">
           <div className="relative">
             <img
               src={myPicture}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,11 @@ const flowbite = require('flowbite-react/tailwind');
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
   theme: {
+    screens: {
+      sm: '853px', // Small screens (mobile)
+      md: '1024px', // Medium screens (tablets)
+      lg: '1366px', // Large screens (laptops and desktops)
+    },
     extend: {
       colors: {
         blueToGreenStart: '#3b82f6',
